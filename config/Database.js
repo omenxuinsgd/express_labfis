bd63aa9bf957b4ce66eb73073545483287fbf5b0
omenxuinsgd <nurrokhman0302@gmail.com>
2024-04-01 13:53:19 +0700

    first commit

---
+++ config/Database.js
@@ -0,0 +1,12 @@
+import {Sequelize} from "sequelize"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    timezone: '+07:00'
+})
+
+export default db
