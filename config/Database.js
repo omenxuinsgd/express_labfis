58554ae267549e461cf1e27b6b7b1ebfb5853f4d
omenxuinsgd <nurrokhman0302@gmail.com>
2024-04-02 17:46:21 +0700

    thirtees commit

--- config/Database.js
+++ config/Database.js
@@ -7,7 +7,14 @@ const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process
     host: process.env.DB_HOST,
     dialect: "mysql",
     timezone: '+07:00',
-    port: 3306
+    port: 3306,
+    pool: {
+        max: 15,
+        min: 5,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000
+      },
 })
 
 export default db
