f925ae735c81cfa3aca7ab48fbf0294ed70f95d8
omenxuinsgd <nurrokhman0302@gmail.com>
2024-04-02 17:35:23 +0700

    fourte commit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(session({
 
 app.use(cors(
     {
-    credentials: true,
+    credentials: false,
     origin: ["http://localhost:3000"],
     // optionSuccessStatus:200, 
     // methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
